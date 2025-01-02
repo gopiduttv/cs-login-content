4e5035901a3065a770c09de5124e841f84383b29
Aravind_Cp <aravindcp@carestack.com>
2025-01-02 16:56:55 +0530

    layout integration with dummy data

--- src/app/campaigns/[campaign]/page.tsx
+++ src/app/campaigns/[campaign]/page.tsx
@@ -8,7 +8,7 @@ export default async function Campaign({ params }: { params: any }) {
   const { campaign: campaignID } = await params;
   const campaign = await runQuery(getCampaignByID(), { campaignID });
   return (
-    <div className="w-5/6 h-[100vh] bg-gradient-to-r from-cyan-500 to-blue-500">
+    <div className="w-full h-[100vh] bg-gradient-to-r from-cyan-500 to-blue-500">
       <CenterText >
         {
           <H2
