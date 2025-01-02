cbfd2ea24d17714a736c6d762310e8bef2994d2a
Princy Saji <princysaji@carestack.com>
2025-01-02 18:05:48 +0530

    banner header changes

--- src/components/templates/Layouts/CenterText.tsx
+++ src/components/templates/Layouts/CenterText.tsx
@@ -1,26 +1,22 @@
-import Banner from "@/components/common/Banners/Banner";
+import Banner from "@/components/common/BannerFooter/Banner";
+import BannerHeader from "@/components/common/BannerHeader/BannerHeader";
 import CTAbutton from "@/components/common/CTAButton";
 import React from "react";
 
 export default function CenterText(props: any) {
-  const paragraphText =
+  const mainTitle =
     "In 2023, the average dental office ran $44,925 in credit card payments per month. ";
+
   return (
     <div
-      className={`${props.className} w-auto h-max flex flex-col items-center pt-72 gap-3`}
+      className={`${props.className} w-auto h-max flex flex-col items-center pt-48 gap-3`}
     >
       {props.children}
-      <h1
-        className={
-          "text-white text-5xl text-center max-w-3xl font-semibold pb-4"
-        }
-      >
-        {paragraphText}
-      </h1>
-      <h3 className="text-3xl text-yellow-600">
-        That's a 45.5% increase over the 2019 average of $30,876.
-      </h3>
-      <CTAbutton className={' rounded-sm text-white font-medium text-center bg-[#2D353E]'} text={"Book Free Demo"}/>
+      <BannerHeader 
+        mainTitle={mainTitle}
+        subTitle="That’s a 45.5% increase over the 2019 average of $30,876."
+        subText="CSPay offers the most streamlined card payments experience for dental offices. With card-on-file, auto-debit and text-to-pay capabilities, stay on step ahead of shifting patient behavior with CSPay"
+        ctaText="Book Free Demo" />
       <Banner
         className=""
         bannerHeading="Inner Circle 2025 - Get Early Bird Tickets!"
