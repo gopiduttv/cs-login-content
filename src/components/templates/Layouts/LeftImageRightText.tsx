c90cd4802b68c16c55c1d770cde4b1a17b9c53e6
Aravind_Cp <aravindcp@carestack.com>
2025-01-02 18:06:56 +0530

    left right image added

---
+++ src/components/templates/Layouts/LeftImageRightText.tsx
@@ -0,0 +1,21 @@
+import Banner from "@/components/common/Banners/Banner";
+import React from "react";
+
+export default function LeftImageRightText(props:any) {
+  return (
+    <div>
+        {
+            props?.children
+        }
+        <section className="max-w-3xl">
+
+        </section>
+      <Banner
+        className=""
+        bannerHeading="Inner Circle 2025 - Get Early Bird Tickets!"
+        bannerText="CS Customers receive an additional $100 off on early bird pricing available through September 13, 2024. Registration is limited to the first 150 dental practices."
+        registerBtnText="Register Now"
+      />
+    </div>
+  );
+}
