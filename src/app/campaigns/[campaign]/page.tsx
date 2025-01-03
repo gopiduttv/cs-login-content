5ee4c89a9cea518f9f50795356db6d081b0ec11a
AravindCarestack <aravindcp@carestack.com>
2025-01-03 12:54:07 +0530

    Sanity setupfor campagin (#8)
    * :sparkles: Basic common Template setup implementation
    
    * layout integration with dummy data
    
    * layout integration with dummy data
    
    * left right image added
    
    * left image layout integration :zap:
    
    * left image layout integration :zap:
    
    * left image layout integration :zap:
    
    * error fixes
    
    * sanity schema added for campaign content :sparkles:

--- src/app/campaigns/[campaign]/page.tsx
+++ src/app/campaigns/[campaign]/page.tsx
@@ -6,18 +6,29 @@ import { getCampaignByID } from "@/sanity/lib/queries";
 import React from "react";
 import LeftImageRightText from "@/components/templates/Layouts/LeftImageRightText";
 import RightImageLeftText from "@/components/templates/Layouts/RightImageLeftText";
+import BannerHeader from "@/components/common/BannerHeader/BannerHeader";
 
 export default async function Campaign({ params }: { params: any }) {
   const { campaign: campaignID } = await params;
   const campaign = await runQuery(getCampaignByID(), { campaignID });
+  const mainTitle =
+    "In 2023, the average dental office ran $44,925 in credit card payments per month. ";
   return (
     <div className="w-full h-[100vh] overflow-hidden bg-gradient-to-r from-cyan-500 to-blue-500">
       <RightImageLeftText>
         {
-          <H2
-            className={" text-yellow-500 text-3xl font-b"}
-            text={campaign.slug.current}
-          />
+          <>
+            <H2
+              className={" text-yellow-500 text-3xl font-b"}
+              text={campaign.slug.current}
+            />
+            <BannerHeader
+              mainTitle={mainTitle}
+              subTitle="That’s a 45.5% increase over the 2019 average of $30,876."
+              subText="CSPay offers the most streamlined card payments experience for dental offices. With card-on-file, auto-debit and text-to-pay capabilities, stay on step ahead of shifting patient behavior with CSPay"
+              ctaText="Book Free Demo"
+            />
+          </>
         }
       </RightImageLeftText>
     </div>
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 
 import { campaign } from "./campaign";
 import { viewport } from "./viewport";
+import { campaignContent } from "./campaignContent";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [campaign, viewport],
+  types: [campaign, viewport, campaignContent],
 };
