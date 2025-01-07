c0626a395b850629bf7783c32063c9338e782dca
Princy Saji <princysaji@carestack.com>
2025-01-07 17:23:10 +0530

    badge added

---
+++ src/utils/page.ts
@@ -0,0 +1,34 @@
+export function formatDate(date: string) {
+    return new Date(date).toLocaleDateString('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    })
+  }
+  
+  export function formatDateChange(dateString: string) {
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+  
+    const [year, month, day] = dateString.split("-");
+    const formattedDate = `${months[parseInt(month, 10) - 1]} ${parseInt(day, 10)}, ${year}`;
+    
+    return formattedDate;
+  }
+
+  export const formatCustomDate = (dateString: string): string => {
+    const date = new Date(dateString);
+  
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'short', // Short weekday (e.g., Tue)
+      day: 'numeric',   // Numeric day (e.g., 9)
+      month: 'short',   // Short month (e.g., May)
+      hour: 'numeric',  // Hour (e.g., 11)
+      minute: 'numeric', // Minutes (e.g., 30)
+      hour12: true      // Use 12-hour format (e.g., am/pm)
+    };
+  
+    return date.toLocaleString('en-US', options);
+  };
