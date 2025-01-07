15ccfdedcbba9d8ada97a8102e1cb00964b72c4d
Princy Saji <princysaji@carestack.com>
2025-01-07 15:07:59 +0530

    block content set to the banner

--- src/components/common/BannerFooter/Banner.tsx
+++ src/components/common/BannerFooter/Banner.tsx
@@ -10,22 +10,36 @@ export interface IBannerInterface {
   registerBtnText?: string;
 }
 
+const customComponents: any = {
+  block: {
+    h2: ({ children }: any) => <h2 className="text-2xl font-bold">{children}</h2>,
+    normal: ({ children }: any) => <p className="pt-4">{children}</p>,
+  },
+  marks: {
+    strong: ({ children }: any) => <strong className="font-bold text-[#42ba78]">{children}</strong>,
+    link: ({ value, children }: any) => (
+      <a href={value?.href} className="text-gray-500 underline text-sm" target="_blank" rel="noopener noreferrer">
+        {children}
+      </a>
+    ),
+  },
+};
+
 export default function Banner({banner}: any) {
+  
   return(
       <Container className={`bg-[${banner?.backgroundColorGradient}] text-white p-${banner?.sectionPadding} rounded-lg`}>
           <div className={`container mx-auto px-${banner?.containerPadding}`}>
             <div className="flex justify-between items-center">
               <div>
-                {/* <h2 className="text-2xl font-bold">{banner?.bannerHeading}</h2> */}
-                <PortableText value={banner?.bannerHeading}/>
+                <PortableText value={banner?.bannerHeading} components={customComponents}/>
                 <span className="text-sm font-light">{banner?.eventLocation}</span>
               </div>
               <a href="#" className={`bg-[${banner?.ctaBtnColor}] text-[${banner?.ctaBtnTextColor}] font-bold py-2 px-4 rounded`}>
                 {banner?.ctaBtnTextForEvent}
               </a>
             </div>
-            {/* <p className="pt-4">{banner?.eventDescription}</p> */}
-            <PortableText value={banner?.eventDescription}/>
+            <PortableText value={banner?.eventDescription}  components={customComponents}/>
           </div>
       </Container>
   )
