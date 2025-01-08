f9eecc8f03c1346037e196472cb7b2026d359456
Gopi Dutt V <gopiduttv@carestack.com>
2025-01-08 10:51:21 +0530

    Fix import issue

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,8 +2,6 @@ import { type SchemaTypeDefinition } from "sanity";
 
 import { campaign } from "./campaign";
 import { viewport } from "./viewport";
-import { campaignContent } from "./campaignContent";
-import { campaignTemplate } from "./CampaignTemplate";
 import { Banner } from "./Banner";
 import { blockContent } from "./objects/BlockContent"; 
 
@@ -11,8 +9,6 @@ export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     campaign,
     viewport,
-    campaignTemplate,
-    campaignContent,
     Banner,
     blockContent,
   ],
