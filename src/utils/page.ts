449887a2f113481d1cc566ffd055c6d8e1208f35
Princy Saji <princysaji@carestack.com>
2025-01-13 12:26:47 +0530

    font import

--- src/utils/page.ts
+++ src/utils/page.ts
@@ -28,6 +28,27 @@ export function formatDateChange(dateString: string) {
   return formattedDate;
 }
 
+export function formatShortDateChange(dateString: string) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const [year, month, day] = dateString.split("-");
+  const formattedDate = `${parseInt(day, 10)} ${months[parseInt(month, 10) - 1]} ${year}`;
+
+  return formattedDate;
+}
 export const formatCustomDate = (dateString: string): string => {
   const date = new Date(dateString);
 
