368841a5758ab9d10fd7af49109ab6f487b2cccb
Princy Saji <princysaji@carestack.com>
2025-01-15 11:35:19 +0530

    style fix

--- src/app/campaigns/[campaign]/ct/page.tsx
+++ src/app/campaigns/[campaign]/ct/page.tsx
@@ -28,7 +28,7 @@ export default async function CenterText({
       <Container
         className={`flex flex-col justify-center max-w-5xl px-4  md:px-8  pt-4 md:pb-8 gap-3 ${banner?.isFullScreen ? "flex-1" : ""}`}
       >
-        <div className="text-center gap-3 pb-8 w-full">
+        <div className="text-center gap-3 pb-8 w-full max-w-[950px] m-auto">
           <CampaignTextArea campaign={campaign} className="w-full justify-items-center" />
         </div>
       </Container>
