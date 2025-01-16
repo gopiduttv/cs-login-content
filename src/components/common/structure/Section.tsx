42de6eadc730e16009c777f87a7e67e096be63df
Princy Saji <princysaji@carestack.com>
2025-01-16 11:29:53 +0530

    background image set

--- src/components/common/structure/Section.tsx
+++ src/components/common/structure/Section.tsx
@@ -4,33 +4,20 @@ type Props = {
   style?: CSSProperties;
   children: ReactNode;
   id?: string | undefined;
-  bgColor?:string;
-  bgImage?:string;
+  bgColor?: string;
+  bgImage?: string;
 };
 
 export default function Section(props: Props) {
   return (
-    <>
-      {props?.bgImage ?
-      <section
+    <section
       id={props?.id}
-      style={{ backgroundImage: `url(${props?.bgImage})` }}
+      style={{
+        background: `${props?.bgColor} , url(${props?.bgImage}) no-repeat center/cover`,
+      }}
       className={`w-full flex flex-col h-screen justify-center relative ${props.className}`}
     >
-      <div className="absolute left-0 top-0 w-full h-full z-10"  style={{background:props?.bgColor}}>
-      </div>
       {props.children}
     </section>
-      :
-      <section
-      id={props?.id}
-      style={{background:props?.bgColor}}
-      className={`w-full flex flex-col h-screen justify-center ${props.className}`}
-    >
-      {props.children}
-    </section>
-      }
-    </>
-     
   );
 }
