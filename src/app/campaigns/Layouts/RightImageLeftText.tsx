74ec2992c560d7902973db1abd7d4911cf92a1a5
Aravind_Cp <aravindcp@carestack.com>
2025-01-21 17:53:49 +0530

    background color schema integration

--- src/app/campaigns/Layouts/RightImageLeftText.tsx
+++ src/app/campaigns/Layouts/RightImageLeftText.tsx
@@ -6,14 +6,24 @@ import CampaignTextArea from "../../components/CampaignTextArea";
 import CampaignImageArea from "../../components/CampaignImageArea";
 import CookieShow from "@/components/common/cookieShow/cookieShow";
 
+interface Color{
+  h1Color:string
+  highlightColor:string
+  paragraphColor:string
+  selectedBgColor:string
+
+}
+
 export default function RightImageLeftText({
   campaign,
   cookies,
   banner = null,
+  colors
 }: {
   campaign: any;
   cookies: any;
   banner: any;
+  colors:Color
 }) {
 
   return (
@@ -28,7 +38,8 @@ export default function RightImageLeftText({
         className={` flex flex-col px-4 md:px-12 pt-4 md:py-8  gap-3  ${banner?.isFullScreen ? "flex-1" : ""}`}
       >
         <div className="flex-grow flex items-center gap-4 lg:gap-24">
-          <CampaignTextArea campaign={campaign} className="flex flex-col max-w-xl xl:max-w-3xl" />
+          <CampaignTextArea campaign={campaign} className="flex flex-col max-w-xl xl:max-w-3xl" 
+          colors={colors}/>
           <CampaignImageArea
             campaignImage={campaign}
             className="items-center max-w-[500px] hidden lg:block"
